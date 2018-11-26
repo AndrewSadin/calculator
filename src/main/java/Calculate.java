1cc99e6b433c8b6deba726d061d465904fc588c5
sa <sa@altsoft.biz>
2018-11-26 09:07:49 +0300

    123

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,15 +1,15 @@
 import java.util.regex.*;
+import javax.script.*;
 
 public class Calculate {
     public static void main(String[] args){
         String calcString = getStringFromFile();
         System.out.println("Start string for calculate " + calcString);
         calcString.replace( " ", "" );
-        if(checkString(calcString)){
-
-        } else {
-
-        };
+        if(checkString(calcString)) {
+            System.out.println(1);
+            calculate(calcString);
+        }
     }
 
     static String getStringFromFile(){
@@ -17,25 +17,32 @@ public class Calculate {
         return fileReader.getString();
     }
 
-    static Boolean checkString(String string){
-        Pattern p = Pattern.compile( "^[0-9*/.,+-]+" );
-        Matcher m = p.matcher( string );
+    static Boolean checkString(String string) {
+        Pattern p = Pattern.compile("^[0-9*/.,+-]+");
+        Matcher m = p.matcher(string);
         if (m.matches()) {
-            System.out.println( "No illegal symbol" );
-            p = Pattern.compile( "^[*/.,]|[+.*/,-]$" );
-            m = p.matcher( string );
-            if(m.matches()) {
+            p = Pattern.compile("^[*/.,]|[+.*/,-]$");
+            if (m.matches()) {
                 return true;
             } else {
+                System.out.println("First or last symbol is not number");
                 return false;
             }
         } else {
+            System.out.println("No illegal symbol");
             return false;
         }
     }
 
-    static String calculate(String string){
-        return string;
+    static void calculate(String string){
+//        String[] numbers = string.split("[/*+.,-]");
+////        for(String number: numbers){
+////            System.out.println(number);
+////        }
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
+        Object result = engine.eval("5*4/2");
+        System.out.println(result);
     }
 
 }
