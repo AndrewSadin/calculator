8d2d6571a2555f2b36551dd69b05213acaa60dc4
sa <sa@altsoft.biz>
2018-12-03 15:50:49 +0300

    slf4j add

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,10 +1,10 @@
 import java.util.regex.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Calculate {
 
-    private static final Logger log = LogManager.getRootLogger();
+    private static Logger log = LoggerFactory.getLogger(Calculate.class);
 
     public static void main(String[] args){
         String calcString = getStringFromFile();
